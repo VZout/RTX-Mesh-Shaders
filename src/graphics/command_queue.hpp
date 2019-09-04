358dcb80aa375f26c59cadf94c06e2beeb57bc7d
Viktor Zoutman <bideblade1@hotmail.com>
2019-09-04 18:22:24 +0200

    Added license information

--- src/graphics/command_queue.hpp
+++ src/graphics/command_queue.hpp
@@ -1,3 +1,9 @@
+/*!
+ *  \author    Viktor Zoutman
+ *  \date      2019-2020
+ *  \copyright GNU General Public License v3.0
+ */
+
 #pragma once
 
 #include "vulkan/vulkan.h"
--- src/graphics/render_window.cpp
+++ src/graphics/render_window.cpp
@@ -1,1 +1,7 @@
+/*!
+ *  \author    Viktor Zoutman
+ *  \date      2019-2020
+ *  \copyright GNU General Public License v3.0
+ */
+
 #include "render_window.hpp"
